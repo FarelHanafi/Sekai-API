0217e83bb12cc31f424383c42783c6ff736c7fce
RellZ <farelcuy122@gmail.com>
2024-08-06 12:49:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const os = require("os");
 const fs = require("fs");
 const masha = require("./function/index");
 const axios = require("axios");
-const favicon = require('serve-favicon');
+const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 
 creator = "Farel Hanafi";
@@ -20,12 +20,8 @@ app.use(cors());
 app.use(secure);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  console.log(`[ ${req.method} ] From: ${req.ip} In ${req.url}`);
-  next();
-});
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname,'/public/logo.png')))
+app.use(favicon(path.join(__dirname, "/public/logo.png")));
 const port = 3000;
 
 // Api Main
@@ -98,20 +94,28 @@ app.get("/api/pinterest", async (req, res) => {
       creator: `${creator}`,
       message: "[!] masukan parameter q",
     });
-  const results = await masha.pinterest(q);
-  try {
-    res.json({
-      status: true,
-      creator: "Farel Hanafi",
-      data: results,
-    });
-  } catch (err) {
-    console.log(err);
-    res.json({ error: "error" });
-  }
+  let result = await masha.pinterest(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
 });
-
-app.get("/api/soundcloud", async (req, res) => {
+app.get("/api/quotesAnime", async (req, res) => {
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.quotesAnime();
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/pixivdl", async (req, res) => {
   let q = req.query.q;
   if (!q)
     return res.json({
@@ -119,20 +123,14 @@ app.get("/api/soundcloud", async (req, res) => {
       creator: `${creator}`,
       message: "[!] masukan parameter q",
     });
-  let result = await masha.soundcloud(q);
-  try {
-    res.json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data: { result },
-    });
-  } catch (err) {
-    console.log(err);
-    res.json(loghandler.error);
-  }
+  let result = await masha.pixivdl(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
 });
-
-app.get("/api/Wikipedia", async (req, res) => {
+app.get("/api/simi", async (req, res) => {
   let q = req.query.q;
   if (!q)
     return res.json({
@@ -140,186 +138,305 @@ app.get("/api/Wikipedia", async (req, res) => {
       creator: `${creator}`,
       message: "[!] masukan parameter q",
     });
-  let result = await masha.Wikipedia(q);
-  try {
-    res.json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data: { result },
+  let result = await masha.simi(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/nhentai", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
     });
-  } catch (err) {
-    console.log(err);
-    res.json(loghandler.error);
-  }
+  let result = await masha.nhentai(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/styletext", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.styletext(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/ringtone", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.ringtone(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/tiktokdl", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.tiktokdl(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/tiktokdl", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.tiktokdl(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/remini", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.remini(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/igstalk", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.igstalk(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/Khodam", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.Khodam(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/GPT4", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.GPT4(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/ttslide", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.ttslide(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/ssweb", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.ssweb(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/wikisearch", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.wikisearch(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/artinama", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.artinama(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/githubstalk", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.githubstalk(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/degreeGuru", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.degreeGuru(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/ragBot", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.ragBot(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/smartContract", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.smartContract(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
+});
+app.get("/api/blackboxAIChat", async (req, res) => {
+  let q = req.query.q;
+  if (!q)
+    return res.json({
+      status: false,
+      creator: `${creator}`,
+      message: "[!] masukan parameter q",
+    });
+  let result = await masha.blackboxAIChat(q);
+  res.json({
+    status: 200,
+    creator: "Farel Hanafi",
+    data: result,
+  });
 });
 
-// AI FEATURES
-app.get("/api/ragbot", async (req, res) => {
-  try {
-    let q = req.query.q;
-    if (!q)
-      return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: "[!] masukan parameter q",
-      });
-      const response = await masha.ragBot(q);
-      res.status(200).json({
-        status: 200,
-        creator: "Farel Hanafi",
-        data: { response },
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-    });
-    
-    app.get("/api/degreeguru", async (req, res) => {
-    try {
-      let q = req.query.q;
-      if (!q)
-        return res.json({
-          status: false,
-          creator: `${creator}`,
-          message: "[!] masukan parameter q",
-        });
-      const response = await masha.degreeGuru(q);
-      res.status(200).json({
-        status: 200,
-        creator: "Farel Hanafi",
-        response,
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-    });
-    
-    app.get("/api/smartcontract", async (req, res) => {
-    try {
-      let q = req.query.q;
-      if (!q)
-        return res.json({
-          status: false,
-          creator: `${creator}`,
-          message: "[!] masukan parameter q",
-        });
-      const response = await masha.smartContract(q);
-      res.status(200).json({
-        status: 200,
-        creator: "Farel Hanafi",
-        data: { response },
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-    });
-    
-    app.get("/api/blackboxAIChat", async (req, res) => {
-    try {
-      let q = req.query.q;
-      if (!q)
-        return res.json({
-          status: false,
-          creator: `${creator}`,
-          message: "[!] masukan parameter q",
-        });
-      const response = await masha.blackboxAIChat(q);
-      res.status(200).json({
-        status: 200,
-        creator: "Farel Hanafi",
-        data: { response },
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-    });
-    
-    app.get("/api/gpt", async (req, res) => {
-    let q = req.query.q;
-    if (!q)
-      return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: "[!] masukan parameter q",
-      });
-    let data = await masha.GPT4(q);
-    res.status(200).json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data,
-    });
-    });
-    
-    // STALK FEATURES
-    app.get("/api/githubstalk", async (req, res) => {
-    let q = req.query.q;
-    if (!q)
-      return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: "[!] masukan parameter q",
-      });
-    let data = await masha.githubstalk(q);
-    res.status(200).json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data,
-    });
-    });
-    
-    app.get("/api/igstalk", async (req, res) => {
-    let q = req.query.q;
-    if (!q)
-      return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: "[!] masukan parameter q",
-      });
-    let data = await masha.igstalk(q);
-    res.status(200).json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data: { data },
-    });
-    });
-    
-    app.get("/api/remini", async (req, res) => {
-    let q = req.query.q;
-    if (!q)
-      return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: "[!] masukan parameter q",
-      });
-    let data = await masha.remini(q);
-    res.status(200).json({
-      status: 200,
-      creator: "Farel Hanafi",
-      data: { data },
-    });
-    });
-    
-    app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "/views/error/404.html"));
-    });
-    
-    app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.sendFile(path.join(__dirname, "/views/error/500.html"));
-    });
-    
-    CFonts.say("Rell API", {
-    font: "block",
-    align: "center",
-    gradient: ["red", "magenta"],
-    });
-    
-    CFonts.say(`Recode By Rell`, {
-    font: "console",
-    align: "center",
-    gradient: ["blue", "yellow"],
-    });
-    
-    app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
-    });
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "/views/error/404.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.sendFile(path.join(__dirname, "/views/error/500.html"));
+});
+
+CFonts.say("Rell API", {
+  font: "block",
+  align: "center",
+  gradient: ["red", "magenta"],
+});
+
+CFonts.say(`Recode By Rell`, {
+  font: "console",
+  align: "center",
+  gradient: ["blue", "yellow"],
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
